44db7a38892acb544e8de0de3d3943429919b9d0
zap-rowsdower <brock.whitbread@gmail.com>
2016-04-30 14:59:33 -0400

    make ajax request work with only jquery

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready', function() {
+    $('#create-tweet').on('click', function(e) {
+        e.preventDefault();
+
+        var tweetData = $('form').serialize();
+
+        $.ajax({
+            url: '/tweets',
+            method: 'POST',
+            dataType: 'html',
+            data: tweetData,
+            success: function(data) {
+                if ( data ) {
+                    $('.tweets').prepend(data);
+                }
+            }
+        });
+    });
+});
