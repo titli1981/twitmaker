6c37da6ce4ee631975f65da4d922f359bba4899c
zap-rowsdower <brock.whitbread@gmail.com>
2016-05-05 19:10:13 -0400

    completed part 1 of bwittergit status

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,6 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('ready', function() {
+
     $('#create-tweet').on('click', function(e) {
         e.preventDefault();
 
@@ -11,11 +12,53 @@ $(document).on('ready', function() {
             method: 'POST',
             dataType: 'html',
             data: tweetData,
-            success: function(data) {
-                if ( data ) {
-                    $('.tweets').prepend(data);
-                }
+            success: function(data_return) {
+                $('.tweets').prepend(data_return);
+                $('textarea').val('');
             }
         });
     });
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#create-tweet').on('click', function(e) {
+//     e.preventDefault();
+//
+//     var tweetData = $('form').serialize();
+//
+//     $.ajax({
+//         url: '/tweets',
+//         method: 'POST',
+//         dataType: 'html',
+//         data: tweetData,
+//         success: function(data) {
+//             if ( data ) {
+//                 $('.tweets').prepend(data);
+//             }
+//         }
+//     });
+// });
