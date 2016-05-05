2808c07046eb1e72514ba1ecbd6e99691115f132
zap-rowsdower <brock.whitbread@gmail.com>
2016-05-05 19:21:33 -0400

    starting JS ajax request

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on("ready", function() {
+    $('#create-tweet').on('click', function(e) {
+        e.preventDefault();
+
+        var tweetData = $('form').serialize();
+
+        $.ajax({
+            url: '/tweets',
+            method: 'POST',
+            dataType: 'script',
+            data: tweetData,
+            success: function(data_return) {
+                
+            }
+        });
+    });
+});
